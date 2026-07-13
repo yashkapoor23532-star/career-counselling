7e5d1703d9c4f5082cd0b7a54fd030618e529c33
yashkapoor23532-star <yashkapoor23532@gmail.com>
2026-07-14 00:47:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+// Welcome message
+console.log("Career Counselling Website Loaded");
 
+// Smooth scroll for internal links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+});
+
+// Book button action
+const btn = document.querySelector("button");
+
+if (btn) {
+  btn.addEventListener("click", function () {
+    alert("Thank you! Our counsellor will contact you soon.");
+  });
+}
